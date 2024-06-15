617df93796c471c1883f462eded454a185ce6c29
Jairo Calcina <104871195+Broukt@users.noreply.github.com>
2024-06-15 00:02:12 -0400

    feat: add register form

---
+++ src/app/_pages/auth/register/register.component.ts
@@ -0,0 +1,164 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styles: [],
+})
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup = new FormGroup({});
+  genderOptions: { value: string; text: string }[] = [
+    { value: '1', text: 'Masculino' },
+    { value: '2', text: 'Femenino' },
+    { value: '3', text: 'Prefiero no decirlo' },
+    { value: '4', text: 'Otro' },
+  ];
+  errorMessage: string = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = this.fb.group({
+      rut: ['', [Validators.required, this.validateRut()]],
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(255),
+        ],
+      ],
+      birthday: ['', [Validators.required, this.validateDate()]],
+      email: ['', [Validators.required, Validators.email]],
+      genderId: [null, [Validators.required]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(20),
+        ],
+      ],
+      confirmPassword: [
+        '',
+        [Validators.required, this.matchValues('password')],
+      ],
+    });
+
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () =>
+        this.registerForm.controls['confirmPassword'].updateValueAndValidity(),
+    });
+  }
+
+  validateRut(): ValidatorFn {
+    return (control: AbstractControl) => {
+      const rut = control.value;
+
+      if (!rut) {
+        return null;
+      }
+
+      const rutPattern = /^[1-9]\d{0,7}-[kK\d]$/;
+      if (!rutPattern.test(rut)) {
+        return { invalidRut: true };
+      }
+
+      const [number, verifier] = rut.split('-');
+
+      let sum = 0;
+      let multiplier = 2;
+
+      for (let i = number.length - 1; i >= 0; i--) {
+        sum += parseInt(number.charAt(i), 10) * multiplier;
+        multiplier = multiplier < 7 ? multiplier + 1 : 2;
+      }
+
+      let calculatedVerifier: string;
+      const modulus = 11 - (sum % 11);
+
+      if (modulus === 11) {
+        calculatedVerifier = '0';
+      } else if (modulus === 10) {
+        calculatedVerifier = 'K';
+      } else {
+        calculatedVerifier = modulus.toString();
+      }
+
+      if (calculatedVerifier.toUpperCase() !== verifier.toUpperCase()) {
+        return { invalidRut: true };
+      }
+
+      return null;
+    };
+  }
+
+  validateDate(): ValidatorFn {
+    return (control: AbstractControl) => {
+      const date = control.value;
+
+      if (!date) {
+        return null;
+      }
+
+      const datePattern =
+        /^(0?[1-9]|[12][0-9]|3[01])\/(0?[1-9]|1[0-2])\/\d{2}$/;
+
+      if (!datePattern.test(date)) {
+        return { invalidDateFormat: true };
+      }
+
+      const [day, month, year] = date.split('/').map(Number);
+
+      const fullYear = 2000 + year;
+      const dateObj = new Date(fullYear, month - 1, day);
+
+      const today = new Date();
+      if (dateObj >= today) {
+        return { futureDate: true };
+      }
+
+      return null;
+    };
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value
+        ? null
+        : { noMatching: true };
+    };
+  }
+
+  register() {
+    this.authService.register(this.registerForm.value).subscribe({
+      next: () => {
+        this.router.navigateByUrl('/');
+      },
+      error: (result) => {
+        if (typeof result.error === 'string') {
+          this.errorMessage = result.error;
+        } else {
+          this.errorMessage = 'Intente nuevamente';
+        }
+      },
+    });
+  }
+}
