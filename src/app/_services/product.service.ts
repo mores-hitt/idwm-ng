b03f3346182f280e3f576a5e7a842313dbaaefc7
Jairo Calcina <104871195+Broukt@users.noreply.github.com>
2024-06-17 11:11:25 -0400

    feat: add product service

---
+++ src/app/_services/product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../_interfaces/product';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  baseUrl: string = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getProducts() {
+    return this.http.get<Product[]>(`${this.baseUrl}/products`);
+  }
+}
