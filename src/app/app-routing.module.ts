6b5a610485181231bd0e4a0bdc30324ec7fa3ea6
Jairo Calcina <104871195+Broukt@users.noreply.github.com>
2024-06-17 20:59:06 -0400

    feat: add products page path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './_pages/auth/login/login.component';
 import { RegisterComponent } from './_pages/auth/register/register.component';
+import { ListComponent } from './_pages/product/list/list.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'products', component: ListComponent },
 ];
 
 @NgModule({
