37b923519bd14c403a777e7e845fdb17bc24af9b
Jairo Calcina <104871195+Broukt@users.noreply.github.com>
2024-06-27 17:39:39 -0400

    feat: add paths for product pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './_pages/auth/login/login.component';
 import { RegisterComponent } from './_pages/auth/register/register.component';
-import { ListComponent } from './_pages/product/list/list.component';
+import { ProductListComponent } from './_pages/product/product-list/product-list.component';
+import { ProductEditComponent } from './_pages/product/product-edit/product-edit.component';
+import { authGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'products', component: ListComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'products',
+        component: ProductListComponent,
+      },
+      {
+        path: 'products/:pageNumber/:pageSize',
+        component: ProductListComponent,
+      },
+      {
+        path: 'product/edit/:id',
+        component: ProductEditComponent,
+      }
+    ],
+  },
 ];
 
 @NgModule({
