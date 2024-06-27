bf2e9dcde0a7a2fa8051feb99275e313e2b9e5bf
Jairo Calcina <104871195+Broukt@users.noreply.github.com>
2024-06-27 17:28:06 -0400

    refactor: change base url for product service

--- src/app/_services/product.service.ts
+++ src/app/_services/product.service.ts
@@ -2,16 +2,17 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
 import { HttpClient } from '@angular/common/http';
 import { Product } from '../_interfaces/product';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  baseUrl: string = environment.apiUrl;
+  baseUrl: string = environment.apiUrl + '/product';
 
   constructor(private http: HttpClient) {}
 
   getProducts() {
-    return this.http.get<Product[]>(`${this.baseUrl}/product`);
+    return this.http.get<Product[]>(`${this.baseUrl}`);
   }
 }
