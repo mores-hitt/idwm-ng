b298ac64a584848632cda796739c75d6dcf726b2
Jairo Calcina <104871195+Broukt@users.noreply.github.com>
2024-06-29 03:51:16 -0400

    style: apply prettier style

--- src/app/_components/search-bar/search-bar.component.ts
+++ src/app/_components/search-bar/search-bar.component.ts
@@ -5,23 +5,26 @@ import { AuthService } from 'src/app/_services/auth.service';
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: [],
 })
 export class SearchBarComponent {
   query: string = '';
   @Output() searchResults = new EventEmitter<any>();
 
-  constructor(private productService: ProductService, private authService: AuthService) {}
+  constructor(
+    private productService: ProductService,
+    private authService: AuthService
+  ) {}
 
-  search(){
-    if(this.authService.getRole() === 'Admin'){
+  search() {
+    if (this.authService.getRole() === 'Admin') {
       this.productService.searchProductAdmin(this.query).subscribe({
         next: (response) => {
           this.searchResults.emit(response);
         },
         error: (result) => {
           //TODO: Handle error
-        }
+        },
       });
     } else {
       this.productService.searchProductUser(this.query).subscribe({
@@ -30,10 +33,8 @@ export class SearchBarComponent {
         },
         error: (result) => {
           //TODO: Handle error
-        }
+        },
       });
     }
   }
-
-
 }
