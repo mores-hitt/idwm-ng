508d75f4ecc766e50abdd28d20b0985e470f84ab
Jairo Calcina <104871195+Broukt@users.noreply.github.com>
2024-06-30 21:08:54 -0400

    feat: add functions to search and edit users

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Receipt } from '../_interfaces/receipt';
 import { User } from '../_interfaces/user';
 import { Observable } from 'rxjs';
 
@@ -18,6 +19,18 @@ export class UserService {
 
   changeStatus(id: number): Observable<string> {
     return this.http.put<string>(`${this.baseUrl}/${id}/state`, null, {
+      responseType: 'text' as 'json',
+    });
+  }
+
+  searchUsers(searchTerm: string): Observable<User[]> {
+    return this.http.get<User[]>(
+      `${this.baseUrl}/search?query=${searchTerm}`
+    );
+  }
+
+  editUser(id: number, model: FormData): Observable<string> {
+    return this.http.put<string>(`${this.baseUrl}/${id}`, model, {
       responseType: 'text' as 'json',
     });
   }
