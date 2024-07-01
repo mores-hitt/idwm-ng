402d845cf81bf62cf53816b77db8645b77915cce
Jairo Calcina <104871195+Broukt@users.noreply.github.com>
2024-06-30 21:05:57 -0400

    feat: add page to show all the clients

--- src/app/_pages/admin/client-list/client-list.component.ts
+++ src/app/_pages/admin/client-list/client-list.component.ts
@@ -2,24 +2,45 @@ import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/_interfaces/user';
 import { UserService } from 'src/app/_services/user.service';
 
+/**
+ * Represents the component for displaying a list of clients.
+ */
 @Component({
   selector: 'app-client-list',
   templateUrl: './client-list.component.html',
   styles: [],
 })
-export class ClientListComponent implements OnInit{
+export class ClientListComponent implements OnInit {
+  /**
+   * The list of users.
+   */
   users: User[] = [];
 
+  /**
+   * Creates an instance of ClientListComponent.
+   * @param userService The user service.
+   */
   constructor(private userService: UserService) {}
 
+  /**
+   * Initializes the component.
+   */
   ngOnInit(): void {
     this.getUsers();
   }
 
+  /**
+   * Gets the status label based on the given status.
+   * @param status The status value.
+   * @returns The status label.
+   */
   getStatus(status: boolean): string {
     return status ? 'Activo' : 'Inactivo';
   }
 
+  /**
+   * Retrieves the list of users from the server.
+   */
   getUsers() {
     this.userService.getUsers().subscribe({
       next: (users) => {
@@ -33,6 +54,10 @@ export class ClientListComponent implements OnInit{
     });
   }
 
+  /**
+   * Changes the status of a user with the given ID.
+   * @param id The ID of the user.
+   */
   changeStatus(id: number) {
     this.users = this.users.map((user) => {
       if (user.id === id) {
@@ -51,5 +76,13 @@ export class ClientListComponent implements OnInit{
         console.log(result);
       },
     });
+  }
+
+  /**
+   * Performs a search based on the given event.
+   * @param event The search event.
+   */
+  search(event: any) {
+    this.users = event;
   }
 }
